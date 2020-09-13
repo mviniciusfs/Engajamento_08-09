eaffaa90ee08683619f1ef32e86e71e5fb168ef4
Marcos_Vinici10 <viniciusfigueiredo@ucl.br>
2020-09-13 13:59:27 +0000

    newv

--- Pessoa.cs
+++ Pessoa.cs
@@ -2,29 +2,89 @@ using System;
 
 class Pessoa
 {
-  public string Nome;
-  public int Idade;
-  public double Peso;
-  public double Altura;
+  private string nome;
+  private int idade;
+  private double peso, altura;
 
-
-  public int Envelhecer(int i)
+  public void Atributos(string n, int i, double p, double a)
   {
-    return Idade + i;
-  } 
-
-  public double Engordar(double p)
-  {
-    return Peso + p;
+    nome = n;
+    idade = i;
+    peso = p;
+    altura = a;
   }
 
-  public double Emagrecer(double p)
+  public void dadosPessoa()
   {
-    return Peso - p;
+    Console.WriteLine("Nome: {0}\nIdade: {1}\nPeso: {2}\nAltura: {3}", nome, idade, peso, altura);
   }
 
-  public double Crescer(double l)
+  public void Envelhecer(int atual)
   {
-    return l + 0.05;
+    if (((atual - idade) > 0) && (idade < 21))
+    {
+      for (int i = atual - idade; i>0; i--)
+      {
+        Crescer(0.005);
+      }
+    }
+
+    if (idade < atual)
+    {
+      idade = atual;
+    }
+
+    Console.WriteLine("Idade atual >> {0}",atual);
   }
+
+  public void Engordar(double engordou)
+  {
+    if(engordou >= peso)
+    {
+      Console.WriteLine("Engordou >> {0} Kg", engordou - peso);
+      peso = engordou;
+      Console.WriteLine("Peso atual >> {0} Kg",peso);
+      peso = engordou;
+    }
+    
+    else
+    {
+      Console.WriteLine("Não engordou!");
+    }
+  }
+
+  public void Emagrecer(double emagreceu)
+  {
+    if(emagreceu < peso)
+    {
+      Console.WriteLine("Emagreceu >> {0} Kg", peso - emagreceu);
+      peso = emagreceu;
+      Console.WriteLine("Peso atual >> {0} Kg",peso);
+    }
+    
+    else
+    {
+      Console.WriteLine("Não emagreceu!");
+    }
+  }
+
+  public void Crescer(double altura2)
+  {
+    if (altura2 == 0.005)
+    {
+      altura += altura2;
+    } 
+    
+    else 
+    {
+      altura = altura2;
+    }
+  }
+
 }
+
+
+
+ 
+
+
--- main.cs
+++ main.cs
@@ -1,14 +1,30 @@
 using System;
 
+/*
+Crie uma classe que modele uma pessoa:
+
+Atributos: nome, idade, peso e altura
+Métodos: Envelhecer, engordar, emagrecer, crescer. Obs: Por padrão, a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.
+*/
+
 class MainClass {
   public static void Main (string[] args) {
     Pessoa p = new Pessoa();
 
-    p.Nome = "Marcos";
-    p.Idade = 20;
-    p.Peso = 85;
-    p.Altura = 1.85;
+    p.Atributos("Teste", 16, 85, 1.83);
 
-    Console.WriteLine(p.Crescer(p.Altura));
+    p.dadosPessoa();    
+
+    Console.WriteLine();
+
+    p.Envelhecer(20);
+    p.Engordar(86);
+    p.dadosPessoa();
+
+    Console.WriteLine();
+
+    p.Emagrecer(80);
+    //p.Crescer(1.85);
+    p.dadosPessoa();
   }
 }
